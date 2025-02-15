08f1f58667669ddf3c1e530cd5ed4b6c1d31bf68
picklemustard <dillon.w.carter@gmail.com>
2025-02-14 19:13:56 -0600

    trying different relaxation methods

--- Structures/Edge.cs
+++ Structures/Edge.cs
@@ -1,4 +1,5 @@
-public class Edge : IEdge
+using System;
+public class Edge : IEdge, IEquatable<Edge>
 {
     public Point P { get; set; }
     public Point Q { get; set; }
@@ -9,6 +10,21 @@ public class Edge : IEdge
         Index = e;
         P = p;
         Q = q;
+    }
+
+    public bool Equals(Edge other)
+    {
+        return other.Index == Index && P == other.P && Q == other.Q;
+    }
+
+    public static bool operator ==(Edge e1, Edge e2)
+    {
+        return e1.Equals(e2);
+    }
+
+    public static bool operator !=(Edge e1, Edge e2)
+    {
+        return !e1.Equals(e2);
     }
 
     public override string ToString() => $"Edge ({Index}, {P}, {Q})";
