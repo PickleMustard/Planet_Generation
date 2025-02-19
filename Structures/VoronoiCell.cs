43b3c5860fbd6ce68fdc767837f3ecc0842ff73f
picklemustard <dillon.w.carter@gmail.com>
2025-02-18 18:23:54 -0600

    Continent generation

--- Structures/VoronoiCell.cs
+++ Structures/VoronoiCell.cs
@@ -1,8 +1,10 @@
+using Godot;
 public class VoronoiCell : IVoronoiCell
 {
     public Point[] Points { get; set; }
     public Triangle[] Triangles { get; set; }
     public int Index { get; set; }
+    public Vector2 MovementDirection { get; set; }
     public VoronoiCell(int triangleIndex, Point[] points, Triangle[] triangles)
     {
         Triangles = triangles;
@@ -10,16 +12,17 @@ public class VoronoiCell : IVoronoiCell
         Index = triangleIndex;
     }
 
-    public override string ToString() {
-      string output = "";
-      output += $"VoronoiCell: ({Index}, ";
-      //foreach(Point p in Points) {
-      //  output += $"{p}, ";
-      //}
-      //foreach(Triangle t in Triangles) {
-      //  output += $"{t}, ";
-      //}
-      output += ")";
-      return output;
+    public override string ToString()
+    {
+        string output = "";
+        output += $"VoronoiCell: ({Index}, ";
+        //foreach(Point p in Points) {
+        //  output += $"{p}, ";
+        //}
+        //foreach(Triangle t in Triangles) {
+        //  output += $"{t}, ";
+        //}
+        output += ")";
+        return output;
     }
 }
