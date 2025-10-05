9f9a595f7d325efd5ee35896f48be4ced903c79a
picklemustard <dillon.w.carter@gmail.com>
2025-10-04 19:31:25 -0500

    Adding satellites to celestial bodies

---
+++ Mesh/player_ship.res
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ab3c24ee8395c342cc4c4fa5049b605ce4ac951332b855234c2075813f03bb1
+size 45658
--- Scripts/MeshGeneration/BiomeAssigner.cs
+++ Scripts/MeshGeneration/BiomeAssigner.cs
@@ -45,8 +45,8 @@ public static class BiomeAssigner
         else if (normalizedHeight > 0.78f) result = BiomeType.Mountain;
         else if (normalizedHeight > 0.4f && (latitude > 0.8f || latitude < -0.8f)) result = BiomeType.Tundra;
         else if (normalizedHeight > 0.4f && (latitude > 0.7f || latitude < -0.7f)) result = BiomeType.Taiga;
-        else if (normalizedHeight < 0.1f) result = BiomeType.Ocean;
-        else if (normalizedHeight < 0.2f) result = BiomeType.Coastal;
+        else if (normalizedHeight < 0.05f) result = BiomeType.Ocean;
+        else if (normalizedHeight < 0.12f) result = BiomeType.Coastal;
         else if (normalizedHeight < 0.3f && moisture < 0.2f) result = BiomeType.Desert;
         else if (normalizedHeight < 0.3f && moisture < 0.5f) result = BiomeType.Grassland;
         else if (normalizedHeight > 0.3f && moisture < 0.7f) result = BiomeType.Forest;
@@ -76,7 +76,7 @@ public static class BiomeAssigner
         float latitudeFactor = Mathf.Clamp(continent.averagedCenter.Y / 9f, 0f, 1f);
         float sizeFactor = continent.cells.Count / 100f;
 
-        float randomVariation = rng.RandfRange(-0.2f, 0.2f);
+        float randomVariation = rng.RandfRange(-0.4f, 0.2f);
         float value = MAX_MOISTURE - (baseMoisture + latitudeFactor + sizeFactor + randomVariation) / MAX_MOISTURE;
         Logger.ExitFunction("CalculateMoisture", $"returned {value:F3}");
         return value;
